ff6ef341022c2834128f5d6faf29b11f01f23ffc
Abhishek Goyal <abhishek.goyal9419@gmail.com>
2018-08-11 17:34:42 +0530

    Initial Setup with UI

---
+++ app/src/main/java/com/example/abhishek/currencyconverter/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.abhishek.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void convert(View v){
+
+        EditText usdEditText = findViewById(R.id.usdText);
+        EditText rsEditText = findViewById(R.id.rsText);
+
+ //       double result = (Double) usdEditText.getText().toString();
+
+
+    }
+
+}
